1a7a200774181f56bb6b5a2c854adf13b92e7309
Jacob Olsen <olsenj8@byu.edu>
2023-03-09 01:55:32 -0700

    mission10

---
+++ BookStore/Components/TypesViewComponent.cs
@@ -0,0 +1,31 @@
+﻿using BookStore_olsenj8.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore_olsenj8.Components
+{
+    public class TypesViewComponent : ViewComponent
+    {
+        //Loading up a dataset
+        private IBookStoreRepository repo { get; set; }
+        public TypesViewComponent (IBookStoreRepository temp)
+        {
+            repo = temp;
+        }
+
+        //Pulling the distinct category types and returning it to a view
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.SelectedCategory = RouteData?.Values["bookCategory"];
+
+            var types = repo.Books
+                .Select(x => x.Category)
+                .Distinct()
+                .OrderBy(x => x);
+            return View(types);
+        }
+    }
+}
--- BookStore/Startup.cs
+++ BookStore/Startup.cs
@@ -36,6 +36,11 @@ namespace BookStore
            });
 
             services.AddScoped<IBookStoreRepository, EFBookStoreRepository>();
+
+            services.AddRazorPages();
+
+            services.AddDistributedMemoryCache();
+            services.AddSession();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -48,13 +53,29 @@ namespace BookStore
 
             //Telling the program to use the files in the wwwroot folder
             app.UseStaticFiles();
+            app.UseSession();
 
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapControllerRoute("categorypage",
+                    "{bookCategory}/Page{pageNum}",
+                    new { Controller = "Home", action = "Index" });
+
+                endpoints.MapControllerRoute(
+                  name: "Paging",
+                  pattern: "Page{pageNum}",
+                  defaults: new { Controller = "Home", action = "Index", pageNum = 1 }
+                  );
+
+                endpoints.MapControllerRoute("category",
+                    "{bookCategory}",
+                    new { Controller = "Home", action = "Index", pageNum = 1 });
+              
                 //Follow MVC route pattern
                 endpoints.MapDefaultControllerRoute();
+                endpoints.MapRazorPages();
             });
         }
     }
